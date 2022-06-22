eeee62ba7dd78ddb755721c371c105085416eec1
NumAniCloud <num.ani.cloud@gmail.com>
2022-06-22 20:10:49 +0900

    IFileSystemOperatorを追加

---
+++ Numani.TypedFilePath/Infrastructure/IFileSystemOperator.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace Numani.TypedFilePath.Infrastructure
+{
+	public interface IFileOperator
+	{
+		FileStream Create(string filePath);
+		void Delete(string filePath);
+		bool Exists(string filePath);
+
+		void WriteAllText(string filePath, string text);
+		void WriteAllBytes(string filePath, byte[] bytes);
+		string ReadAllText(string filePath);
+		byte[] ReadAllBytes(string filePath);
+	}
+}
